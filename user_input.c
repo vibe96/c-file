d5ac358b3b3ec6093a456d3c225f1fbc301d7448
vibe96 <olalekanazeez96@gmail.com>
2023-10-14 01:41:01 +0100

    user_input commit

--- user_input.c
+++ user_input.c
@@ -7,7 +7,7 @@ int main(){
     printf("Input amount : \n");
     scanf("%d", &price); //&price is the address of the operator
 
-    printf("You are paying $%d :\n", price30);
+    printf("You are paying $%d :\n", price);
 
 
     return 0;
